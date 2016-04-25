ec85592392dce2b7632f21e0db65db175a00bfbb
japaz <juanantonio.pazsalgado@telefonica.com>
2016-04-25 19:57:52 +0200

    Initial commit

--- firefox/speeder.py
+++ firefox/speeder.py
@@ -1,10 +1,11 @@
 class SpeederClass(object):
 
-    def __init__(self, name):
+    def __init__(self, name, weight):
         self.name = name
+        self.weight = weight
 
     def __str__(self):
-        return self.name
+        return self.name + '(%s kg)' % self.weight
 
 class Speeder(object):
 
@@ -14,7 +15,7 @@ class Speeder(object):
         self.parts = []
 
     def __str__(self):
-        result = '%s (%s):' % (self.name, self.kind.name)
+        result = '%s (%s):\n Total weight: %skg' % (self.name, self.kind.name, self.kind.weight)
 
         for part in self.parts:
             result += '\n' + part
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from firefox.speeder import Speeder,SpeederClass
 
 
 if __name__ == "__main__":
-    kind = SpeederClass('Klase Trash')
-    speeder = Speeder('Cometa Rojo',kind)
+    kind = SpeederClass('Klase Trash', 600)
+    speeder = Speeder('Cometa Rojo', kind)
 
     print speeder
